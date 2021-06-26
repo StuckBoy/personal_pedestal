21137ba7fb6369490716e43eb4b1ebb991ad75ad
StuckBoy <zstuck96@gmail.com>
2021-06-26 16:26:39 -0400

    Added Basic Bio Page With Redirect on Landing Page

--- src/main/java/com/stuckboy/PersonalWebsite/PersonalWebsiteApplication.java
+++ src/main/java/com/stuckboy/PersonalWebsite/PersonalWebsiteApplication.java
@@ -2,29 +2,29 @@ package com.stuckboy.PersonalWebsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class PersonalWebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalWebsiteApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String landingPage() {
-		return """
-					This is the default landing page for the website.
-					
-					Additional functionality to be implemented at a later date.
-				""";
+	@RequestMapping(value ="/", method = RequestMethod.GET)
+	@ResponseBody
+	public ModelAndView landingPage() {
+		return new ModelAndView("landingPage");
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	@RequestMapping(value = "/bio.html", method = RequestMethod.GET)
+	@ResponseBody
+	public ModelAndView bioPage() {
+		return new ModelAndView("bio");
 	}
 }
